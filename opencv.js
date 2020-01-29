b2861e56f8902141a9a69e808f5d476818dff099
Damir Petrov <petrovdamir2235@gmail.com>
2020-01-29 23:53:24 +0300

    Adds image decoding

--- opencv.js
+++ opencv.js
@@ -38,12 +38,55 @@ ws.onerror = function (err) {
   console.log('err: ', err);
 }
 
+var beg = 0;
+let buf = "";
+
 ws.onmessage = function (event) {
   //console.log(event.data);
-  draw2(event.data);
+  buf += event.data;
+  console.log(buf.length);
+  im = read_jpeg();
+  while (im != undefined) {
+    draw2(im);
+    im = read_jpeg();
+  }
 };
 
 ws.onclose = function() {
   console.log("Connection is closed...");
 }
 
+function read_jpeg() {
+  var started = false;
+  for (; beg + 1 < buf.length; beg++) {
+    if (buf[beg] == String.fromCharCode(255)[0] &&
+        buf[beg + 1] == String.fromCharCode(16 * 13 + 8)[0]) {
+      started = true;
+      break;
+    }
+  }
+  
+  if (!started) {
+    return undefined;
+  }
+  
+  console.log('LoL');
+  var end = beg;
+  var finished = false;
+  for (; end + 1 < lenght(buf); end++) {
+    if (buf[end] == String.fromCharCode(255)[0] &&
+        buf[end + 1] == String.fromCharCode(16 * 13 + 9)) {
+      finished = true;
+      break;
+    }
+  }
+  
+  if (!finished) {
+    return undefined;
+  }
+
+  let im = buf.substr(beg, end + 1);
+  buf = buf.substr(end + 2, buf.length - 1);
+  beg = 0;
+  return im;
+}
--- video.js
+++ video.js
@@ -1,12 +1,3 @@
-/*
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-*/
-
 var stdin = process.openStdin();
 
 const WebSocketServer = require('ws').Server
